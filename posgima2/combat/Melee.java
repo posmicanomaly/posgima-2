473de0e4c1316ffee6d7a2342fbb970a2d31e9e0
posmicanomaly <eqkakg@gmail.com>
2015-01-22 13:03:26 -0500

    Combat for now is now based solely off min/max hit, no strength involved. No more AC check for landing a hit, its now percentage based off agility and dexterity. 1:1 ratio. Right now everyone is missing at level 1, but this might make balance easier.

--- posgima2/combat/Melee.java
+++ posgima2/combat/Melee.java
@@ -23,27 +23,52 @@ public abstract class Melee {
         If hitRoll less than defender's AC or hitroll equals 1, miss
          */
 
-        if (hitRoll < defender.getTotalArmorClass() + defender.getAgility() || hitRoll == 1) {
+        /*
+        NEW:
+        AC for damage reduction
+        Agility and Dexterity for missing.
+        Agile = harder to hit
+        Dextrous = Easier to land hit
+        % base, 0-100, so 10 agility = 10% chance to dodge, 10% dexterity = 10% chance to hit.
+        This might result in a lot of misses early on.
+         */
+        int attackDexterityRoll = (int)(Math.random() * 100);
+        int defendAgilityRoll = (int)(Math.random() * 100);
+        boolean agilityCheck = false;
+        if(defendAgilityRoll < defender.getAgility()) {
+            /*
+            e.g. roll: 10, agility 23, successful agility check
+             */
+            agilityCheck = true;
+        }
+        boolean dexterityCheck = false;
+        if(attackDexterityRoll < attacker.getDexterity()) {
+            dexterityCheck = true;
+        }
+        /*
+        If defender was agile enough, and attacker failed dexterity check
+        MISS
+         */
+        if((agilityCheck && !dexterityCheck) ||
+                !agilityCheck && !dexterityCheck){
             if(ANNOUNCE) {
                 WindowFrame.writeConsole("/combat/" + attacker + " missed " + defender + ".");
             }
-            if(SYSTEM_CONSOLE_OUTPUT) {
-                System.out.println(attacker + " missed " + defender);
-            }
             attacker.setAttackedThisTurn(true);
         }
-
         /*
-        If hitroll greater than defender's AC or hitroll equals 20, hit
+        If attacker was dextrous, and defender not agile, or if they both passed, the more dextrous will land a hit
          */
-        else if(hitRoll > defender.getTotalArmorClass() || hitRoll == 20) {
-
+        else if((dexterityCheck && !agilityCheck) ||
+                dexterityCheck && agilityCheck) {
             int str = attacker.getStrength();
             int dmgbonus = attacker.getDamageBonus();
             int minHitDamage = attacker.getMinHitDamage();
             int maxHitDamage = attacker.getMaxHitDamage();
-
-            int damageRoll = (int) ((Math.random() * (maxHitDamage - minHitDamage)) + minHitDamage + (str / 2) + dmgbonus);
+            int strComponent = (str / 2);
+            // Test, remove str
+            strComponent = 0;
+            int damageRoll = (int) ((Math.random() * (maxHitDamage - minHitDamage)) + minHitDamage + strComponent + dmgbonus);
             int defenseRoll = (int)(Math.random() * defender.getTotalArmorClass()) + 1;
             int actualDamage = damageRoll - defenseRoll;
             /*
@@ -73,6 +98,59 @@ public abstract class Melee {
                 System.out.println(output);
             }
         }
+//        }
+//        if (hitRoll < defender.getTotalArmorClass() + defender.getAgility() || hitRoll == 1) {
+//            if(ANNOUNCE) {
+//                WindowFrame.writeConsole("/combat/" + attacker + " missed " + defender + ".");
+//            }
+//            if(SYSTEM_CONSOLE_OUTPUT) {
+//                System.out.println(attacker + " missed " + defender);
+//            }
+//            attacker.setAttackedThisTurn(true);
+//        }
+//
+//        /*
+//        If hitroll greater than defender's AC or hitroll equals 20, hit
+//         */
+//        else if(hitRoll > defender.getTotalArmorClass() || hitRoll == 20) {
+//
+//            int str = attacker.getStrength();
+//            int dmgbonus = attacker.getDamageBonus();
+//            int minHitDamage = attacker.getMinHitDamage();
+//            int maxHitDamage = attacker.getMaxHitDamage();
+//            int strComponent = (str / 2);
+//            // Test, remove str
+//            strComponent = 0;
+//            int damageRoll = (int) ((Math.random() * (maxHitDamage - minHitDamage)) + minHitDamage + strComponent + dmgbonus);
+//            int defenseRoll = (int)(Math.random() * defender.getTotalArmorClass()) + 1;
+//            int actualDamage = damageRoll - defenseRoll;
+//            /*
+//            actual damage guaranteed to be 1
+//             */
+//            if(actualDamage < 1) {
+//                actualDamage = 1;
+//            }
+//
+//            defender.applyDamage(actualDamage);
+//            /*
+//            Check death
+//             */
+//            if(!defender.isAlive()) {
+//                defender.die();
+//            }
+//
+//            attacker.setAttackedThisTurn(true);
+//            String output = attacker + " hit " + defender + " for " + actualDamage + "(" +
+//                    (damageRoll - actualDamage) + " absorbed). (" + defender
+//                    .getCurrentHP() + "/" + defender.getMaxHP() + ") [L" + defender.getLevel() + "]";
+//
+//            if(ANNOUNCE) {
+//                WindowFrame.writeConsole("/combat/" + output);
+//            }
+//            if(SYSTEM_CONSOLE_OUTPUT) {
+//                System.out.println(output);
+//            }
+//        }
     }
 
     public static void meleeCombat(Entity one, Entity two, boolean twoCanAttack) {
